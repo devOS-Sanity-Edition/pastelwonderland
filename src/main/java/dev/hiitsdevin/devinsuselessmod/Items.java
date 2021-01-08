94f9b98850a4f13b4cac546f5c99478b742ff8f0
Devin Louis <devin@hiitsdevin.dev>
2021-01-08 03:40:05 -0800

    Blocks can now be placed via Item

--- src/main/java/dev/hiitsdevin/devinsuselessmod/Items.java
+++ src/main/java/dev/hiitsdevin/devinsuselessmod/Items.java
@@ -3,6 +3,7 @@ package dev.hiitsdevin.devinsuselessmod;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.BlockItem;
+import net.minecraft.block.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -27,17 +28,16 @@ public class Items {
         return new Item.Settings().group(DEVIN_GROUP);
     }
 
-    public static final Item RED = new Item(itemSettings());
-    public static final Item ORANGE = new Item(itemSettings());
-    public static final Item YELLOW = new Item(itemSettings());
-    public static final Item GREEN = new Item(itemSettings());
-    public static final Item BLUE = new Item(itemSettings());
-    public static final Item PURPLE = new Item(itemSettings());
-    public static final Item MAGENTA = new Item(itemSettings());
-    public static final Item BLACK = new Item(itemSettings());
-    public static final Item GRAY = new Item(itemSettings());
-    public static final Item MICHAEL_REEVES = new Item(itemSettings());
-
+    public static final Item RED = new BlockItem(Blocks.RED, itemSettings());
+    public static final Item ORANGE = new BlockItem(Blocks.ORANGE, itemSettings());
+    public static final Item YELLOW = new BlockItem(Blocks.YELLOW, itemSettings());
+    public static final Item GREEN = new BlockItem(Blocks.GREEN, itemSettings());
+    public static final Item BLUE = new BlockItem(Blocks.BLUE, itemSettings());
+    public static final Item PURPLE = new BlockItem(Blocks. PURPLE, itemSettings());
+    public static final Item MAGENTA = new BlockItem(Blocks.MAGENTA, itemSettings());
+    public static final Item BLACK = new BlockItem(Blocks.BLACK, itemSettings());
+    public static final Item GRAY = new BlockItem(Blocks.GRAY, itemSettings());
+    public static final Item MICHAEL_REEVES = new BlockItem(Blocks.MICHAEL_REEVES, itemSettings());
 
     private static Item register(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier("devin", name), item);
