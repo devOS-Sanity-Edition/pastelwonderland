1b220060566c554fceae22381925d715a4f7299e
Devin Louis <devin@hiitsdevin.dev>
2021-01-23 07:53:34 -0800

    Plants can now be... planted.

---
+++ src/main/java/dev/hiitsdevin/pastelwonderland/Mixin/PlantBlockMixin.java
@@ -0,0 +1,19 @@
+package dev.hiitsdevin.pastelwonderland.Mixin;
+
+import dev.hiitsdevin.pastelwonderland.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlantBlock.class)
+public class PlantBlockMixin {
+    @Inject(method = "canPlantOnTop", at = @At("TAIL"), cancellable = true)
+    protected void canPlantOnTop(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(cir.getReturnValue() || (floor.isOf(Blocks.GRASS) || (floor.isOf(Blocks.DIRT))));
+    }
+}
