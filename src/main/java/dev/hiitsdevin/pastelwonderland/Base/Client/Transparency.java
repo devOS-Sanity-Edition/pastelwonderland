9138be7e7e0ef14557c2cac580ed26d0e0477d52
Devin Louis <devin@hiitsdevin.dev>
2021-01-27 05:01:31 -0800

    translations done [for now]

--- src/main/java/dev/hiitsdevin/pastelwonderland/Base/Client/Transparency.java
+++ src/main/java/dev/hiitsdevin/pastelwonderland/Base/Client/Transparency.java
@@ -7,6 +7,8 @@ import net.minecraft.client.render.RenderLayer;
 public class Transparency {
     public static void init() {
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.RED_LEAF, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.RED_LEAF, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.BLUE_LEAF, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.YELLOW_LEAF, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.GREEN_LEAF, RenderLayer.getCutout());
     }
 }
