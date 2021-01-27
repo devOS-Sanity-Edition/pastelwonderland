d491c759cde4ac84a40570c9df7cdb811d346165
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2021-01-27 16:55:53 -0700

    teeny weensy change

--- src/main/java/dev/hiitsdevin/pastelwonderland/PastelWonderland.java
+++ src/main/java/dev/hiitsdevin/pastelwonderland/PastelWonderland.java
@@ -10,6 +10,7 @@ import dev.hiitsdevin.pastelwonderland.Entities.Mallards.MallardEntity;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
@@ -21,9 +22,10 @@ import net.minecraft.util.registry.Registry;
 public class PastelWonderland implements ModInitializer {
 
     // for the text render. be sure to comment out both HudRenderCallsbacks when you're not in a dev env.
-    public static final String PrototypePastelVersion = "Pastel Wonderland - Development Build v0.0.2";
-    public static final String PrototypePastelDistribution = "If this build is not public please do not redistribute it!";
     public static final String MOD_ID = "pastelwonderland";
+    public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().toString();
+    public static final String PrototypePastelVersion = "Pastel Wonderland - Dev Build: " + VERSION;
+    public static final String PrototypePastelDistribution = "If this build is not public, please do not redistribute it!";
 
     public static final EntityType<MallardEntity> MALLARD = Registry.register(
             Registry.ENTITY_TYPE,
