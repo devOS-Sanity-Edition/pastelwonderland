37fb2f8269284dbc04ce6d6735c50338379f9d4d
Devin Louis <devin@hiitsdevin.dev>
2021-01-28 19:50:08 -0800

    Mallard AI + Adding Armours and Tools

--- src/main/java/dev/hiitsdevin/pastelwonderland/Base/DevDisclaimer.java
+++ src/main/java/dev/hiitsdevin/pastelwonderland/Base/DevDisclaimer.java
@@ -8,25 +8,25 @@ import net.minecraft.client.MinecraftClient;
 public class DevDisclaimer {
     public static void init() {
         // disclaimer text only shows in dev environment as of jan 27th. production build doesnt have it.
-        //if (FabricLoader.INSTANCE.isDevelopmentEnvironment()) {}
-        HudRenderCallback.EVENT.register((matrices, delta) -> {
-            MinecraftClient.getInstance().textRenderer
-                    .drawWithShadow(matrices, PastelWonderland.PastelWonderlandVersion, 1, 1, 0xFFFFFF); // shoot me where it hurts most please.
-        });
-
-        // renders Distribution text
-        HudRenderCallback.EVENT.register((matrices, delta) -> {
-            MinecraftClient.getInstance().textRenderer
-                    .drawWithShadow(matrices, PastelWonderland.PastelWonderlandDistribution, 1, 10, 0xFFFFFF);
-        });
-
-        // Game FPS
-        HudRenderCallback.EVENT.register((matrices, delta) -> {
-            MinecraftClient.getInstance().textRenderer
-                    .drawWithShadow(matrices, PastelWonderland.PastelWonderlandFPSDevelopment, 1, 20, 0xFFFFFF);
-        });
-
         if (FabricLoader.INSTANCE.isDevelopmentEnvironment()) {
+            HudRenderCallback.EVENT.register((matrices, delta) -> {
+                MinecraftClient.getInstance().textRenderer
+                        .drawWithShadow(matrices, PastelWonderland.PastelWonderlandVersion, 1, 1, 0xFFFFFF); // shoot me where it hurts most please.
+            });
+
+            // renders Distribution text
+            HudRenderCallback.EVENT.register((matrices, delta) -> {
+                MinecraftClient.getInstance().textRenderer
+                        .drawWithShadow(matrices, PastelWonderland.PastelWonderlandDistribution, 1, 10, 0xFFFFFF);
+            });
+
+            // Game FPS
+            HudRenderCallback.EVENT.register((matrices, delta) -> {
+                MinecraftClient.getInstance().textRenderer
+                        .drawWithShadow(matrices, PastelWonderland.PastelWonderlandFPSDevelopment, 1, 20, 0xFFFFFF);
+            });
+
+
             // renders Distribution text
             HudRenderCallback.EVENT.register((matrices, delta) -> {
                 MinecraftClient.getInstance().textRenderer
@@ -35,3 +35,4 @@ public class DevDisclaimer {
         }
     }
 }
+
