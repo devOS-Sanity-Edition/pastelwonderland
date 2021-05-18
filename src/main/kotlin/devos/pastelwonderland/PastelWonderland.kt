d7c3c9c9edbc9a20d11a4d5232f695a84e258f0b
Siuol <Lmeitrodt@gmail.com>
2021-05-17 20:39:40 -0500

    Bricks and blocks on nox on box
    In this commit I do a little trolling :troll:

--- src/main/kotlin/devos/pastelwonderland/PastelWonderland.kt
+++ src/main/kotlin/devos/pastelwonderland/PastelWonderland.kt
@@ -17,6 +17,8 @@ class PastelWonderland : ModInitializer {
     // maybe one day, tater will be a thing here again, no promises.
     // it's been months since this but i dont think im adding back tater. i still feel heavily discouraged after the first time. i'm sorry.
     // while doing this kotlin rewrite... im thinking about it, albeit, still rly sad ngl.
+
+    // Aye Devin, while it would be nice to add it back, its fine, besides, we could instead add "pineapples" as thats the quilt meme
     override fun onInitialize() {
         Blocks.init()
         Items.init()
@@ -24,7 +26,8 @@ class PastelWonderland : ModInitializer {
         ToolsAndArmor.init()
 
         // comment this thing below out lol
-        if (net.fabricmc.loader.FabricLoader.INSTANCE.isDevelopmentEnvironment()) { DevDisclaimer.init() }
+        // no lmfao
+        if (FabricLoader.getInstance().isDevelopmentEnvironment) { DevDisclaimer.init() }
     }
 
     companion object {
