ba7bc310c63106dcd857551aeb3019015dd665a9
HiItsDevin <30705254+HiItsDevin@users.noreply.github.com>
2021-06-11 03:31:59 -0700

    1.17 Port, no portals for now.

--- src/main/java/devos/pastelwonderland/mixin/isSoilMixin.java
+++ src/main/java/devos/pastelwonderland/mixin/isSoilMixin.java
@@ -4,16 +4,13 @@ import devos.pastelwonderland.Base.Gameplay.Blocks;
 import net.minecraft.block.Block;
 import net.minecraft.world.gen.feature.Feature;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Feature.class)
 public class isSoilMixin {
-    @Inject(method = "isSoil(Lnet/minecraft/block/Block;)Z", at = @At(value = "HEAD"), cancellable = true)
     private static void isSoil(Block block, CallbackInfoReturnable<Boolean> cir)
     {
-        if (block == Blocks.LIGHT_RED) {
+        if (block == Blocks.INSTANCE.getPASTEL_DIRT() || block == Blocks.INSTANCE.getPASTEL_GRASS()) {
             cir.setReturnValue(true);
         }
     }
--- src/main/kotlin/devos/pastelwonderland/Base/Gameplay/Portals.kt
+++ src/main/kotlin/devos/pastelwonderland/Base/Gameplay/Portals.kt
@@ -1,25 +1,21 @@
 package devos.pastelwonderland.Base.Gameplay
 
-import devos.pastelwonderland.PastelWonderland.Companion.PW_MOD_ID
-import net.kyrptonaught.customportalapi.CustomPortalApiRegistry
-import net.minecraft.util.Identifier
-
 object Portals {
     fun init() {
         // registers portal ability, probably more blocks soon, idk.
-        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_RED, Identifier(PW_MOD_ID, "pastel_dim"), 253, 174, 187)
-        CustomPortalApiRegistry.addPortal(Blocks.RED, Identifier(PW_MOD_ID, "pastel_dim"), 216, 65, 37)
-        CustomPortalApiRegistry.addPortal(Blocks.ORANGE, Identifier(PW_MOD_ID, "pastel_dim"), 255, 187, 164)
-        CustomPortalApiRegistry.addPortal(Blocks.YELLOW, Identifier(PW_MOD_ID, "pastel_dim"), 243, 241, 161)
-        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_GREEN, Identifier(PW_MOD_ID, "pastel_dim"), 109, 213, 195)
-        CustomPortalApiRegistry.addPortal(Blocks.GREEN, Identifier(PW_MOD_ID, "pastel_dim"), 28, 123, 80)
-        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_BLUE, Identifier(PW_MOD_ID, "pastel_dim"), 116, 209, 234)
-        CustomPortalApiRegistry.addPortal(Blocks.BLUE, Identifier(PW_MOD_ID, "pastel_dim"), 28, 123, 80)
-        CustomPortalApiRegistry.addPortal(Blocks.PURPLE, Identifier(PW_MOD_ID, "pastel_dim"), 191, 155, 222)
-        CustomPortalApiRegistry.addPortal(Blocks.MAGENTA, Identifier(PW_MOD_ID, "pastel_dim"), 241, 178, 220)
-        CustomPortalApiRegistry.addPortal(Blocks.BLACK, Identifier(PW_MOD_ID, "pastel_dim"), 63, 63, 63)
-        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_GRAY, Identifier(PW_MOD_ID, "pastel_dim"), 191, 191, 191)
-        CustomPortalApiRegistry.addPortal(Blocks.GRAY, Identifier(PW_MOD_ID, "pastel_dim"), 28, 123, 80)
-        CustomPortalApiRegistry.addPortal(Blocks.BROWN, Identifier(PW_MOD_ID, "pastel_dim"), 191, 68, 20)
+//        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_RED, Identifier(PW_MOD_ID, "pastel_dim"), 253, 174, 187)
+//        CustomPortalApiRegistry.addPortal(Blocks.RED, Identifier(PW_MOD_ID, "pastel_dim"), 216, 65, 37)
+//        CustomPortalApiRegistry.addPortal(Blocks.ORANGE, Identifier(PW_MOD_ID, "pastel_dim"), 255, 187, 164)
+//        CustomPortalApiRegistry.addPortal(Blocks.YELLOW, Identifier(PW_MOD_ID, "pastel_dim"), 243, 241, 161)
+//        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_GREEN, Identifier(PW_MOD_ID, "pastel_dim"), 109, 213, 195)
+//        CustomPortalApiRegistry.addPortal(Blocks.GREEN, Identifier(PW_MOD_ID, "pastel_dim"), 28, 123, 80)
+//        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_BLUE, Identifier(PW_MOD_ID, "pastel_dim"), 116, 209, 234)
+//        CustomPortalApiRegistry.addPortal(Blocks.BLUE, Identifier(PW_MOD_ID, "pastel_dim"), 28, 123, 80)
+//        CustomPortalApiRegistry.addPortal(Blocks.PURPLE, Identifier(PW_MOD_ID, "pastel_dim"), 191, 155, 222)
+//        CustomPortalApiRegistry.addPortal(Blocks.MAGENTA, Identifier(PW_MOD_ID, "pastel_dim"), 241, 178, 220)
+//        CustomPortalApiRegistry.addPortal(Blocks.BLACK, Identifier(PW_MOD_ID, "pastel_dim"), 63, 63, 63)
+//        CustomPortalApiRegistry.addPortal(Blocks.LIGHT_GRAY, Identifier(PW_MOD_ID, "pastel_dim"), 191, 191, 191)
+//        CustomPortalApiRegistry.addPortal(Blocks.GRAY, Identifier(PW_MOD_ID, "pastel_dim"), 28, 123, 80)
+//        CustomPortalApiRegistry.addPortal(Blocks.BROWN, Identifier(PW_MOD_ID, "pastel_dim"), 191, 68, 20)
     }
 }
