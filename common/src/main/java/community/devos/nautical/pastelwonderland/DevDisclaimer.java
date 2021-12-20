f3b6f14bd3d4120fae2b5c1225cd05b4cc81288c
Devin <devin@hiitsdevin.dev>
2021-12-20 02:39:49 -0800

    GPG Commit Signing test + Optimizing Imports

--- common/src/main/java/community/devos/nautical/pastelwonderland/DevDisclaimer.java
+++ common/src/main/java/community/devos/nautical/pastelwonderland/DevDisclaimer.java
@@ -1,8 +1,5 @@
 package community.devos.nautical.pastelwonderland;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Minecraft;
-
 public class DevDisclaimer {
     public static void init() {
         if (PastelWonderland.devEnv) {
