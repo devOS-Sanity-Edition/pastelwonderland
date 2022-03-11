68d5959e5d1989c9f4050208292fa5063aa4a1db
Devin <asoji-git@hiitsdevin.mozmail.com>
2022-03-10 21:16:41 -0800

    ➖ Knocked out REI for now. Game launches now.

--- src/main/java/community/devos/nautical/pastelwonderland/rei/HiddenItemRemover.java
+++ src/main/java/community/devos/nautical/pastelwonderland/rei/HiddenItemRemover.java
@@ -1,18 +1,18 @@
-package community.devos.nautical.pastelwonderland.rei;
-
-import community.devos.nautical.pastelwonderland.common.HiddenItems;
-import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
-import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
-import me.shedaniel.rei.api.common.util.EntryStacks;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-@Environment(EnvType.CLIENT)
-public class HiddenItemRemover implements REIClientPlugin {
-
-    @Override
-    public void registerEntries(EntryRegistry registry) {
-        registry.removeEntry(EntryStacks.of(HiddenItems.LOGO));
-        registry.removeEntry(EntryStacks.of(HiddenItems.RED_BLOCK));
-    }
-}
+//package community.devos.nautical.pastelwonderland.rei;
+//
+//import community.devos.nautical.pastelwonderland.common.HiddenItems;
+//import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+//import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
+//import me.shedaniel.rei.api.common.util.EntryStacks;
+//import net.fabricmc.api.EnvType;
+//import net.fabricmc.api.Environment;
+//
+//@Environment(EnvType.CLIENT)
+//public class HiddenItemRemover implements REIClientPlugin {
+//
+//    @Override
+//    public void registerEntries(EntryRegistry registry) {
+//        registry.removeEntry(EntryStacks.of(HiddenItems.LOGO));
+//        registry.removeEntry(EntryStacks.of(HiddenItems.RED_BLOCK));
+//    }
+//}
