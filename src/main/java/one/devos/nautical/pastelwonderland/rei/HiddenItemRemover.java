69ee3eaf0795566a1f1f4ff4809c6a81cacd679e
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-04-12 04:30:47 -0700

    🔨 Added back REI support

--- src/main/java/one/devos/nautical/pastelwonderland/rei/HiddenItemRemover.java
+++ src/main/java/one/devos/nautical/pastelwonderland/rei/HiddenItemRemover.java
@@ -1,18 +1,18 @@
-//package one.devos.nautical.pastelwonderland.rei;
-//
-//import HiddenItems;
-//import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
-//import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
-//import me.shedaniel.rei.api.common.util.EntryStacks;
-//import net.fabricmc.api.EnvType;
-//import net.fabricmc.api.Environment;
-//
-//@Environment(EnvType.CLIENT)
-//public class HiddenItemRemover implements REIClientPlugin {
-//
-//    @Override
-//    public void registerEntries(EntryRegistry registry) {
-//        registry.removeEntry(EntryStacks.of(HiddenItems.LOGO));
-//        registry.removeEntry(EntryStacks.of(HiddenItems.RED_BLOCK));
-//    }
-//}
+package one.devos.nautical.pastelwonderland.rei;
+
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import one.devos.nautical.pastelwonderland.common.HiddenItems;
+
+@Environment(EnvType.CLIENT)
+public class HiddenItemRemover implements REIClientPlugin {
+
+    @Override
+    public void registerEntries(EntryRegistry registry) {
+        registry.removeEntry(EntryStacks.of(HiddenItems.LOGO));
+        registry.removeEntry(EntryStacks.of(HiddenItems.RED_BLOCK));
+    }
+}
