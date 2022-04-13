37eef1891bc79d2c44ebeb625d139edf6793d025
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-04-12 23:52:04 -0700

    🍱 More food textures

--- src/main/java/one/devos/nautical/pastelwonderland/common/Music.java
+++ src/main/java/one/devos/nautical/pastelwonderland/common/Music.java
@@ -1,12 +1,5 @@
 package one.devos.nautical.pastelwonderland.common;
 
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.Musics;
-import net.minecraft.sounds.SoundEvent;
-
-import static one.devos.nautical.pastelwonderland.Pastelwonderland.MOD_ID;
-
 public class Music {
     //idk how to rly add sounds lol, someone figure this out.
 }
